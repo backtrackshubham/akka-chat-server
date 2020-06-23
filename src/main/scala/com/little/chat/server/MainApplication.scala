1f4e1adc3491c4b5ebf27b90a130cee7b4548d02
shubham <shubham.verma@knoldus.com>
2020-06-23 21:38:10 +0530

    Fixed host

--- src/main/scala/com/little/chat/server/MainApplication.scala
+++ src/main/scala/com/little/chat/server/MainApplication.scala
@@ -64,7 +64,7 @@ object MainApplication extends App {
     }
   }
 
-  val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+  val bindingFuture = Http().bindAndHandle(route, "0.0.0.0", 8080)
 
   println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
   StdIn.readLine() // let it run until user presses return
